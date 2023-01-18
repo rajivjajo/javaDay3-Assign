826e8282a95b72cf9621a8e58cb9fda06fb5b3f4
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-18 19:24:42 +0530

    LineComparison using Oops

--- src/com/bridgelab/assignmentdaye/LengthBW2points.java
+++ src/com/bridgelab/assignmentdaye/LengthBW2points.java
@@ -3,7 +3,9 @@ package com.bridgelab.assignmentdaye;
 import java.util.Scanner;
 
 public class LengthBW2points {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("enter x1 ");
         int x1 = sc.nextInt();
@@ -17,16 +19,23 @@ public class LengthBW2points {
         int x3 = sc.nextInt();
         System.out.println("enter y3 ");
         int y3 = sc.nextInt();
-        double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        double length1 = Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
-
-
+        Double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        Double length1 = Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
         System.out.println("Length between co-ordinates" + "(" + x1 + "," + y1 + ")," + "(" + x2 + "," + y2 + ") is : " + length);
         System.out.println("Length between co-ordinates" + "(" + x1 + "," + y1 + ")," + "(" + x3 + "," + y3 + ") is : " + length1);
-        if (length1 == length)
-            System.out.println("Two lines are equal in length");
-        else {
-            System.out.println("Two lines are not equal");
+        compareline(length, length1);
+    }
+
+    public static void compareline(Double length, Double length1) {
+        System.out.println("Length of line are : " + length + "and" + length1);
+        if (length.compareTo(length1) > 0) {
+            System.out.println(length + " > " + length1);
+        } else if (length.compareTo(length1) < 0) {
+            System.out.println(length + " < " + length1);
+        } else {
+            System.out.println(length + " = " + length1);
         }
     }
 }
+
+
